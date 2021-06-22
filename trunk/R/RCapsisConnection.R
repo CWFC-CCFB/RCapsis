84df4ec98dc1b377647a65d52be0f249b53f68d1
donenrique <donenrique@daf9b3f9-8859-402d-b080-51404485b33c>
2021-06-22 15:55:45 +0000

    Improving connection to CAPSIS

--- trunk/R/RCapsisConnection.R
+++ trunk/R/RCapsisConnection.R
@@ -12,33 +12,40 @@
 #' This environment contains the general settings of the package.
 #'
 #' @export
-settingEnv <- new.env()
+rcapsisSettingEnv <- new.env()
 
 
 #' @export
-connectToCapsis <- function() {
+connectToCapsis <- function(memorySize = NULL) {
   if (J4R::isConnectedToJava()) {
-    message("R is already connected to Java. The library will be added dynamically. This could have an expected behaviour if the libraries are already in the classpath!")
-    capsisPath <- getCapsisPath()
-    J4R::addToClassPath(file.path(getCapsisPath(), "class"))
-    extensionPath <- file.path(getCapsisPath(), "ext")
-    extensionFiles <- dir(extensionPath)
-    for (filename in extensionFiles) {
-      if (endsWith(filename, ".jar")) {
-        J4R::addToClassPath(file.path(extensionPath, filename))
-      }
+    message("R is already connected to Java. RCapsis will add capsis extensions dynamically if need be.")
+  } else {
+    J4R::connectToJava(memorySize = memorySize)
+  }
+  .addCapsisClasspath()
+  return(TRUE)
+}
+
+
+.addCapsisClasspath <- function() {
+  capsisPath <- getCapsisPath()
+  J4R::addToClassPath(file.path(getCapsisPath(), "class"))
+  extensionPath <- file.path(getCapsisPath(), "ext")
+  extensionFiles <- dir(extensionPath)
+  for (filename in extensionFiles) {
+    if (endsWith(filename, ".jar")) {
+      J4R::addToClassPath(file.path(extensionPath, filename))
     }
   }
 }
 
 
-
 #' @export
 getCapsisPath <- function() {
-  if (!exists("capsisPath", envir = settingEnv, inherits = F)) {
+  if (!exists("capsisPath", envir = rcapsisSettingEnv, inherits = F)) {
     stop("It seems the path to CAPSIS has not been set. Please use the setCapsisPath function.")
   } else {
-    capsisPath <- get("capsisPath", envir = settingEnv, inherits = F)
+    capsisPath <- get("capsisPath", envir = rcapsisSettingEnv, inherits = F)
     if (!dir.exists(capsisPath)) {
       stop("It seems the path to CAPSIS does not exist or is not a directory. Please use the setCapsisPath function to set the correct path to your local distribution of the CAPSIS platform.")
     }
@@ -66,7 +73,17 @@ setCapsisPath <- function(path) {
   } else if (!dir.exists(path)) {
     stop("The path argument does not point to a directory!")
   }
-  assign("capsisPath", path, envir = settingEnv, inherits = F)
+  assign("capsisPath", path, envir = rcapsisSettingEnv, inherits = F)
 }
 
 
+#'
+#' Create an instance of C4Script for a particular model
+#'
+#' @param  modelName the anme of the model (e.g. artemis)
+#'
+#' @export
+createC4ScriptInstance <- function(modelName) {
+  myScript <- J4R::createJavaObject("capsis.app.C4Script", modelName)
+  return(myScript)
+}
